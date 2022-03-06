2ad044d7009eefbe6095a64e89c7be3c5f513f58
Nghiem Truong <vinhnghiemcr@gmail.com>
2022-03-05 23:03:52 -0600

    Created Post Parse Object model

--- app/src/main/java/com/example/instagram/ParseApplication.java
+++ app/src/main/java/com/example/instagram/ParseApplication.java
@@ -3,12 +3,16 @@ package com.example.instagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        //register parse model
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("VURaj58f0Q5uLSVWL8VAsvPMIKQLQaI5USayVHml")
                 .clientKey("Kw7OfN0xM3cYlAXV3qs3JoaAgyGduJRgvGjAKydk")
